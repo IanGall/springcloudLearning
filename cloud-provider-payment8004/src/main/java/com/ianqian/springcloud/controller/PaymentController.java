f7b0070e33859bfd019ed66125e77e7566bc17fe
IanQian <qianyifan1996@gmail.com>
2021-11-09 21:21:47 +0800

    支付服务注册进zookeeper

---
+++ cloud-provider-payment8004/src/main/java/com/ianqian/springcloud/controller/PaymentController.java
@@ -0,0 +1,30 @@
+package com.ianqian.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @author IanQian
+ * @date 2021/11/9 20:38
+ */
+@RestController
+@RequestMapping("/payment")
+@Slf4j
+public class PaymentController {
+    @Value("${server.port}")
+    private String serverPort;
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping("/zk")
+    public String paymentzk() {
+        return "springcloud with zookeeper: "+serverPort+"\t"+ UUID.randomUUID();
+    }
+}
