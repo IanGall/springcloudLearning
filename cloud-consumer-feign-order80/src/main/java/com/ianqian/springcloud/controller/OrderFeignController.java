65500360d490f65b0bf774884d8c89dc2ec719fc
IanQian <qianyifan1996@gmail.com>
2021-11-13 17:33:53 +0800

    OpenFeign服务调用

---
+++ cloud-consumer-feign-order80/src/main/java/com/ianqian/springcloud/controller/OrderFeignController.java
@@ -0,0 +1,29 @@
+package com.ianqian.springcloud.controller;
+
+import com.ianqian.springcloud.entities.CommonResult;
+import com.ianqian.springcloud.entities.Payment;
+import com.ianqian.springcloud.service.PaymentFeignService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author IanQian
+ * @date 2021/11/13 14:14
+ */
+@RestController
+@RequestMapping("/consumer")
+@Slf4j
+public class OrderFeignController {
+
+    @Autowired
+    private PaymentFeignService paymentFeignService;
+
+    @GetMapping("/payment/get/{id}")
+    public CommonResult<Payment> getPaymentId(@PathVariable("id") String id) {
+        return paymentFeignService.selectByPrimaryKey(id);
+    }
+}
---
+++ cloud-consumer-feign-order80/src/main/java/com/ianqian/springcloud/service/PaymentFeignService.java
@@ -0,0 +1,19 @@
+package com.ianqian.springcloud.service;
+
+import com.ianqian.springcloud.entities.CommonResult;
+import com.ianqian.springcloud.entities.Payment;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * @author IanQian
+ * @date 2021/11/13 13:29
+ */
+@Component
+@FeignClient("CLOUD-PAYMENT-SERVICE")
+public interface PaymentFeignService {
+    @GetMapping("/payment/get/{id}")
+    CommonResult<Payment> selectByPrimaryKey(@PathVariable("id") String id);
+}
