afa5f2b970ba4549fe44a3df5a903695f2519dbb
IanQian <qianyifan1996@gmail.com>
2021-11-14 12:56:54 +0800

    Hystrix之全局服务降级DefaultProperties

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/ianqian/springcloud/controller/OrderHystrixController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/ianqian/springcloud/controller/OrderHystrixController.java
@@ -1,6 +1,7 @@
 package com.ianqian.springcloud.controller;
 
 import com.ianqian.springcloud.service.OrderHystrixService;
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/consumer")
 @Slf4j
+@DefaultProperties(defaultFallback = "payment_Global_FallbackMethod")
 public class OrderHystrixController {
 
     @Autowired
@@ -28,9 +30,10 @@ public class OrderHystrixController {
     }
 
     @GetMapping("/hystrix/timeout/{id}")
-    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler", commandProperties = {
-            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000")
-    })
+    // @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler", commandProperties = {
+    //         @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000")
+    // })
+    @HystrixCommand//用全局的fallback方法
     public String paymentInfo_TimeOut(@PathVariable("id") Integer id) {
         return orderHystrixService.paymentInfo_TimeOut(id);
     }
@@ -39,4 +42,10 @@ public class OrderHystrixController {
         return "线程池:" + Thread.currentThread().getName() + "paymentInfo_TimeOutHandler," +
                 "id:" + "\t" + "::>_<::";
     }
+
+    // 下面是全局fallback方法
+    public String payment_Global_FallbackMethod() {
+        return "Global异常处理信息，请稍后再试，/(ㄒoㄒ)/~~";
+    }
+
 }
