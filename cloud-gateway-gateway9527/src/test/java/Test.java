146a7d4b1aaa5b2e4ae52ab1511c71bd16db15af
IanQian <qianyifan1996@gmail.com>
2021-11-14 21:31:50 +0800

    GateWay常用的Predicate

---
+++ cloud-gateway-gateway9527/src/test/java/Test.java
@@ -0,0 +1,12 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @author IanQian
+ * @date 2021/11/14 20:48
+ */
+public class Test {
+    public static void main(String[] args) {
+        ZonedDateTime now = ZonedDateTime.now();
+        System.out.println(now);
+    }
+}
