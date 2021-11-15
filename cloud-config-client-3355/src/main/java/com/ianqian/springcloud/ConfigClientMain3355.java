9b164f6d5d23014d5ca423fa2dded7f615e9f462
IanQian <qianyifan1996@gmail.com>
2021-11-15 21:23:58 +0800

    Config客户端配置与测试

---
+++ cloud-config-client-3355/src/main/java/com/ianqian/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,18 @@
+package com.ianqian.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author IanQian
+ * @date 2021/11/15 20:48
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigClientMain3355
+{
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+}
