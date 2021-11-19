972e121002652d4e7aa84a84fc3ef86748efae7d
IanQian <qianyifan1996@gmail.com>
2021-11-19 20:07:21 +0800

    Nacos之服务配置中心

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/ianqian/springcloud/NacosConfigClientMain3377.java
@@ -0,0 +1,17 @@
+package com.ianqian.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author IanQian
+ * @date 2021/11/18 23:26
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NacosConfigClientMain3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class, args);
+    }
+}
